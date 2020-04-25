cd4f744c64371646f1a3c3306a7d5a7cffd71bbf
bonamerigo <seanonuoha@yahoo.com>
2020-04-25 11:00:24 +0100

    lesson 8

--- app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
+++ app/src/main/java/com/example/swoosh/Controller/LeagueActivity.kt
@@ -1,10 +1,11 @@
-package com.example.swoosh
+package com.example.swoosh.Controller
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.example.swoosh.Utilities.EXTRA_LEAGUE
+import com.example.swoosh.R
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
@@ -38,7 +39,7 @@ class LeagueActivity : BaseActivity() {
         if (selectedLeague != "") {
             val skillActivity = Intent(this, SkillActivity::class.java)
             skillActivity.putExtra(EXTRA_LEAGUE, selectedLeague)
-            startActivity(SkillActivity)
+            startActivity(skillActivity)
         } else {
             Toast.makeText(this, "Biko select a league.", Toast.LENGTH_SHORT).show()
         }
